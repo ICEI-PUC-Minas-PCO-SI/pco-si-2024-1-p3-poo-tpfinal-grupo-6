aeabc7e49a7feb7630c5832627d5e2a7acdb860d
Carol <carolinaabpessoa@gmail.com>
2024-06-13 17:02:21 -0300

    criação do domínio do Partido

---
+++ src/UrnaEletronica/backend/UrnaEletronica.Dominio/Modelos/Partidos/Partido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrnaEletronica.Dominio.Modelos.Cidades;
+
+namespace UrnaEletronica.Dominio.Modelos.Partidos
+{
+    public class Partido
+    {
+        public int Id { get; set; }
+        public int CodigoPartido { get; set; }
+        public string Nome { get; set; }
+        public string Sigla { get; set; }
+        public IEnumerable<Cidade> Cidades { get; set; }
+    }
+}
