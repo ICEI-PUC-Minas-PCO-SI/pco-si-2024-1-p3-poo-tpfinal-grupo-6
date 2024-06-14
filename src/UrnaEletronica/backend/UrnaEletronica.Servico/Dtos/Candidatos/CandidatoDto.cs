1e7a1f8c214aad4bfa3498ef8eff275eb0540a07
Alex de Souza Galdino <alexsgaldino@gmail.com>
2024-06-14 16:37:47 -0300

    implementação do serviço para candidato

---
+++ src/UrnaEletronica/backend/UrnaEletronica.Servico/Dtos/Candidatos/CandidatoDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrnaEletronica.Dominio.Modelos.Cidades;
+using UrnaEletronica.Dominio.Modelos.Partidos;
+using UrnaEletronica.Servico.Dtos.Cidades;
+
+namespace UrnaEletronica.Servico.Dtos.Candidatos
+{
+    public class CandidatoDto
+    {
+        public int Id { get; set; }
+        public int ExecutivoId { get; set; }
+        public int LegislativoId { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public int QtdVotos { get; set; }
+        public int CidadeId { get; set; }
+        public CidadeDto Cidade { get; set; }
+        public int PartidoId { get; set; }
+        //public PartidoDto Partido { get; set; }
+    }
+}
