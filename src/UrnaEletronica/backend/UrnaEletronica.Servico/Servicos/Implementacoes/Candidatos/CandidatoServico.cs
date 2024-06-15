a0bcc79ff1ff317be62883c150ef3a3c65fd8d3f
alexgaldinopuc <126251768+MatheusFelips@users.noreply.github.com>
2024-06-15 12:18:43 -0300

    Squashed commit of the following:
    commit 4862d25fc4437eb3912d0f9f6baffb5e15d5bc4c
    Merge: e0a7f78 c4549f3
    Author: igoranunes <127458065+igoranunes@users.noreply.github.com>
    Date:   Fri Jun 14 22:15:43 2024 -0300
    
        Merge pull request #17 from ICEI-PUC-Minas-PCO-SI/dev/igor
    
        atualizandocoligacoes
    
    commit c4549f35d1b1bf48975b74b613f85814a0e45815
    Author: igoranunes <igaoanunes@gmail.com>
    Date:   Fri Jun 14 22:14:46 2024 -0300
    
        atualizandocoligacoes
    
    commit e0a7f782843e4fae88a389e7aaeb0e5683fad9ff
    Author: igoranunes <127458065+igoranunes@users.noreply.github.com>
    Date:   Fri Jun 14 21:06:40 2024 -0300
    
        Update nome  README.md
    
    commit 4cdb984367b3ef99e2a5a3d6d178626611d65c90
    Merge: a8dbb6b 1e7a1f8
    Author: alexgaldinopuc <94413402+alexgaldinopuc@users.noreply.github.com>
    Date:   Fri Jun 14 16:38:24 2024 -0300
    
        Merge pull request #16 from ICEI-PUC-Minas-PCO-SI/dev/candidatoservico
    
        implementação do serviço para candidato
    
    commit 1e7a1f8c214aad4bfa3498ef8eff275eb0540a07
    Author: Alex de Souza Galdino <alexsgaldino@gmail.com>
    Date:   Fri Jun 14 16:37:47 2024 -0300
    
        implementação do serviço para candidato

---
+++ src/UrnaEletronica/backend/UrnaEletronica.Servico/Servicos/Implementacoes/Candidatos/CandidatoServico.cs
@@ -0,0 +1,151 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using UrnaEletronica.Dominio.Modelos.Candidatos;
+using UrnaEletronica.Persistencia.Interfaces.Contratos.Candidatos;
+using UrnaEletronica.Servico.Dtos.Candidatos;
+using UrnaEletronica.Servico.Servicos.Contratos.Candidatos;
+
+namespace UrnaEletronica.Servico.Servicos.Implementacoes.Candidatos
+{
+    public class CandidatoServico : ICandidatoServico
+    {
+        private readonly ICandidatoPersistencia _candidatoPersistencia;
+        private readonly IMapper _mapper;
+
+        public CandidatoServico(ICandidatoPersistencia candidatoPersistencia, IMapper mapper)
+        {
+            _candidatoPersistencia = candidatoPersistencia;
+            _mapper = mapper;
+        }
+        public async Task<CandidatoDto> CreateCandidato(CandidatoDto candidatoDto)
+        {
+            try
+            {
+                var candidato = _mapper.Map<Candidato>(candidatoDto);
+
+                _candidatoPersistencia.Create<Candidato>(candidato);
+
+                if (await _candidatoPersistencia.SaveChangeAsync())
+                {
+                    var candidatoRetorno = await _candidatoPersistencia.GetCandidatoByIdAsync(candidato.Id);
+
+                    return _mapper.Map<CandidatoDto>(candidatoRetorno);
+                }
+
+                return null;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public async Task<bool> DeleteCandidato(int candidatoId)
+        {
+            try
+            {
+                var candidato = await _candidatoPersistencia.GetCandidatoByIdAsync(candidatoId);
+
+                if (candidato == null)
+                    throw new Exception("Candidato para exclusão náo foi encontrado!");
+
+                _candidatoPersistencia.Delete<Candidato>(candidato);
+
+                return await _candidatoPersistencia.SaveChangeAsync();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public async Task<IEnumerable<CandidatoDto>> GetAllCandidatosAsync()
+        {
+            try
+            {
+                var candidatos = await _candidatoPersistencia.GetAllCandidatosAsync();
+
+                if (candidatos == null) return null;
+
+                var candidatosMappper = _mapper.Map<CandidatoDto[]>(candidatos);
+
+                return candidatosMappper;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public async Task<CandidatoDto> GetCandidatoByIdAsync(int candidatoId)
+        {
+            try
+            {
+                var candidato = await _candidatoPersistencia.GetCandidatoByIdAsync(candidatoId);
+
+                if (candidato == null) return null;
+
+                var candidatoMapper = _mapper.Map<CandidatoDto>(candidato);
+
+                return candidatoMapper;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public async Task<bool> RegistrarVoto(int candidatoId)
+        {
+            var candidato = await _candidatoPersistencia.GetCandidatoByIdAsync(candidatoId);
+
+            if (candidato == null) return false;
+
+            var votoRegistrado = await _candidatoPersistencia.RegistrarVoto(candidatoId);
+
+            return votoRegistrado;
+        }
+
+        public async Task<bool> RegistrarVoto(int candidatoId, int qtdVotos)
+        {
+            var candidato = await _candidatoPersistencia.GetCandidatoByIdAsync(candidatoId);
+
+            if (candidato == null) return false;
+
+            var votoRegistrado = await _candidatoPersistencia.RegistrarVoto(candidatoId, qtdVotos);
+
+            return votoRegistrado;
+        }
+
+        public async Task<CandidatoDto> UpdateCandidato(int candidatoId, CandidatoDto candidatoDto)
+        {
+            try
+            {
+                var candidato = await _candidatoPersistencia.GetCandidatoByIdAsync(candidatoId);
+
+                if (candidato == null) return null;
+
+                var candidatoUpdate = _mapper.Map(candidatoDto, candidato);
+
+                _candidatoPersistencia.Update<Candidato>(candidatoUpdate);
+
+                if (await _candidatoPersistencia.SaveChangeAsync())
+                {
+                    var candidatoMapper = await _candidatoPersistencia.GetCandidatoByIdAsync(candidatoMapper.Id);
+                    return _mapper.Map<CandidatoDto>(candidatoMapper);
+                }
+
+                return null;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
