877d086dae84a25cafbc1b2787ba4d752bd53bca
igoranunes <igaoanunes@gmail.com>
2024-06-18 22:12:19 -0300

    att dev/igor

--- src/UrnaEletronica/backend/UrnaEletronica.Persistencia/Interfaces/Implementacoes/Coligacoes/ColigacaoPersistencia.cs
+++ src/UrnaEletronica/backend/UrnaEletronica.Persistencia/Interfaces/Implementacoes/Coligacoes/ColigacaoPersistencia.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UrnaEletronica.Dominio.Modelos.Cidades;
+using UrnaEletronica.Dominio.Modelos.Coligacoes;
+using UrnaEletronica.Persistencia.Contexto;
+using UrnaEletronica.Persistencia.Interfaces.Contratos.Cidades;
+using UrnaEletronica.Persistencia.Interfaces.Contratos.Coligacoes;
+using UrnaEletronica.Persistencia.Interfaces.Implementacoes.Shared;
 
 namespace UrnaEletronica.Persistencia.Interfaces.Implementacoes.Coligacoes
 {
-    internal class ColigacaoPersistencia
+    public class ColigacaoPersistencia : SharedPersistencia, IColigacaoPersistencia
     {
+
+            private readonly UrnaEletronicaContexto _contexto;
+
+            public ColigacaoPersistencia(UrnaEletronicaContexto contexto) : base(contexto)
+            {
+                _contexto = contexto;
+            }
+
+            public async Task<IEnumerable<Coligacao>> GetAllCidadesAsync()
+            {
+                IQueryable<Coligacao> query = _contexto.Coligacoes
+                    .AsNoTracking()
+                    .OrderBy(c => c.Id);
+
+                return await query.ToListAsync();
+            }
+
+            public async Task<Coligacao> GetCidadeByIdAsync(int coligacaoId)
+            {
+                IQueryable<Coligacao> query = _contexto.Coligacoes
+                  .AsNoTracking()
+                  .OrderBy(u => u.Id)
+                  .Where(u => u.Id == coligacaoId);
+
+                return await query.FirstOrDefaultAsync();
+            }
+        }
     }
 }
