c042307d7f0b21cfbb6e9e234e6a2c57684acae6
Alex de Souza Galdino <alexsgaldino@gmail.com>
2024-06-23 09:17:40 -0300

    Review Cidade 2

--- src/UrnaEletronica/frontend/UrnaEletronica-app/src/app/pages/cidades/cidades.component.ts
+++ src/UrnaEletronica/frontend/UrnaEletronica-app/src/app/pages/cidades/cidades.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cidades',
   templateUrl: './cidades.component.html',
-  styleUrls: ['./cidades.component.scss']
 })
 export class CidadesComponent implements OnInit {
 
