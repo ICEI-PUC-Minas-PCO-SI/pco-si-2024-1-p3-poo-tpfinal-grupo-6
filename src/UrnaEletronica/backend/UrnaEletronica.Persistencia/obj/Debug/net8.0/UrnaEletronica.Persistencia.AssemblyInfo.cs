66529885bb93aaa49ee741650f0486d8a407ce1a
Carol <carolinaabpessoa@gmail.com>
2024-06-22 23:06:31 -0300

    criação do partido

--- src/UrnaEletronica/backend/UrnaEletronica.Persistencia/obj/Debug/net8.0/UrnaEletronica.Persistencia.AssemblyInfo.cs
+++ src/UrnaEletronica/backend/UrnaEletronica.Persistencia/obj/Debug/net8.0/UrnaEletronica.Persistencia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrnaEletronica.Persistencia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b87f31dc338ea483273bd85fa6ba63983bb5f174")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrnaEletronica.Persistencia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrnaEletronica.Persistencia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
